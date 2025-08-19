efe50b76130c007327980f2419754f37f76717c4
loic <loic@code-rhapsodie.fr>
2025-08-19 08:46:00 +0200

    Add  billing address filter

---
+++ src/Core/FieldComparator/BillingAddressComparator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CodeRhapsodie\IbexaDataflowBundle\Core\FieldComparator;
+
+use Ibexa\Contracts\Core\FieldType\Value;
+
+class BillingAddressComparator extends AbstractFieldComparator
+{
+
+    /**
+     * @param \Ibexa\FieldTypeAddress\FieldType\Value $currentValue
+     * @param \Ibexa\FieldTypeAddress\FieldType\Value $newValue
+     */
+    protected function compareValues(Value $currentValue, Value $newValue): bool
+    {
+        return empty(array_diff_assoc($currentValue->fields, $newValue->fields)) && $currentValue->name !== $newValue->name && $currentValue->country !== $newValue->country;
+    }
+}
