9ed9e842984d29f222c84799e6e2295875165949
loic <loic@code-rhapsodie.fr>
2025-08-19 09:01:13 +0200

    Add  customer group filter

--- src/Core/FieldComparator/BillingAddressComparator.php
+++ src/Core/FieldComparator/BillingAddressComparator.php
@@ -13,6 +13,6 @@ class BillingAddressComparator extends AbstractFieldComparator
      */
     protected function compareValues(Value $currentValue, Value $newValue): bool
     {
-        return empty(array_diff_assoc($currentValue->fields, $newValue->fields)) && $currentValue->name !== $newValue->name && $currentValue->country !== $newValue->country;
+        return empty(array_diff_assoc($currentValue->fields, $newValue->fields)) && $currentValue->name === $newValue->name && $currentValue->country === $newValue->country;
     }
 }
